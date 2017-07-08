ce2aae9607a8b2efad8a888cb0f84e279f2d9f5c
EliCDavis <eli.davis1995@gmail.com>
2017-07-08 03:34:44 -0500

    made dist folder it's own package

--- src/conditionally-validate.module.ts
+++ src/conditionally-validate.module.ts
@@ -1,11 +1,7 @@
 import { ConditionallyValidateService } from './conditionally-validate.service';
 
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms'
-
-export function cvFactory(): ConditionallyValidateService {
-    return new ConditionallyValidateService();
-}
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
     providers: [
